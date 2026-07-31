574d4177eed98d6b1048755d23d10212063b8aa3
harshitkumargupta <149725714+harshitkumargupta@users.noreply.github.com>
2026-08-01 02:35:19 +0530

    Delete DecoratorPatternExample directory

--- DecoratorPatternExample/DecoratorTest.java
+++
@@ -1,9 +0,0 @@
-public class DecoratorTest {
-    public static void main(String[] args) {
-        Notifier notifier = new SlackNotifierDecorator(
-                                new SMSNotifierDecorator(
-                                    new EmailNotifier()));
-
-        notifier.send("System alert!");
-    }
-}
