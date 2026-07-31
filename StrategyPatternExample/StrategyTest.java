c93e3b9a1424c205aa11196ef3266d1af393b9cc
harshitkumargupta <149725714+harshitkumargupta@users.noreply.github.com>
2026-08-01 02:39:38 +0530

    Delete StrategyPatternExample directory

--- StrategyPatternExample/StrategyTest.java
+++
@@ -1,9 +0,0 @@
-public class StrategyTest {
-    public static void main(String[] args) {
-        PaymentContext context = new PaymentContext(new CreditCardPayment());
-        context.executePayment(1200);
-
-        context.setPaymentStrategy(new PayPalPayment());
-        context.executePayment(2500);
-    }
-}
