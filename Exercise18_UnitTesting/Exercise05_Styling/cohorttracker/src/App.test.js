3a9d8d1986f7e253725c743077cd4510dc11cf9a
harshitkumargupta <149725714+harshitkumargupta@users.noreply.github.com>
2026-08-01 02:38:39 +0530

    Delete Exercise18_UnitTesting directory

--- Exercise18_UnitTesting/Exercise05_Styling/cohorttracker/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Cohorts Details heading', () => {
-  render(<App />);
-  const heading = screen.getByText(/Cohorts Details/i);
-  expect(heading).toBeInTheDocument();
-});
