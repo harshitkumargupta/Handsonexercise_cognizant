f85af1c26b2dd569267a5c8a3ffa6d01b10bb1c5
harshitkumargupta <149725714+harshitkumargupta@users.noreply.github.com>
2026-08-01 02:35:28 +0530

    Delete DependencyInjectionExample directory

--- DependencyInjectionExample/DependencyInjectionTest.java
+++
@@ -1,8 +0,0 @@
-public class DependencyInjectionTest {
-    public static void main(String[] args) {
-        CustomerRepository repository = new CustomerRepositoryImpl();
-        CustomerService service = new CustomerService(repository);
-
-        System.out.println(service.getCustomerById(1));
-    }
-}
